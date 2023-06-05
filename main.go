8a366a20b924bbfe4c5a5abedcb42fee3844b3c7
Pedro Cechetto <pedrocechetto@hotmail.com>
2023-06-05 14:53:54 -0300

    select cases

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
 type CDNApiCep struct {
 	Cep        string `json:"cep"`
 	State      string `json:"state"`
@@ -24,8 +32,60 @@ type ViaCep struct {
 	Siafi       string `json:"siafi"`
 }
 
-func main() {
-	ch1 := make(chan CDNApiCep)
-	ch2 := make(chan ViaCep)
+func makeAPIRequest(url string, ch chan<- []byte) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("method 'get' error:", err)
+		ch <- nil
+		return
+	}
+	defer resp.Body.Close()
 
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Read body error", err)
+		ch <- nil
+		return
+	}
+
+	ch <- body
+}
+
+func main() {
+	cep := "69915-630"
+	apicep := "https://cdn.apicep.com/file/apicep/" + cep + ".json"
+	viacep := "http://viacep.com.br/ws/" + cep + "/json/"
+
+	ch1 := make(chan []byte)
+	ch2 := make(chan []byte)
+
+	go makeAPIRequest(apicep, ch1)
+	go makeAPIRequest(viacep, ch2)
+
+	select {
+	case res1 := <-ch1:
+		if res1 != nil {
+			var address1 CDNApiCep
+			err := json.Unmarshal(res1, &address1)
+			if err != nil {
+				fmt.Println("Unmarshal error on cdn.apicep.com: ", err)
+			} else {
+				fmt.Println("cdn.apicep.com:")
+				fmt.Printf("%+v\n", address1)
+			}
+		}
+	case res2 := <-ch2:
+		if res2 != nil {
+			var address2 ViaCep
+			err := json.Unmarshal(res2, &address2)
+			if err != nil {
+				fmt.Println("Unmarshall error viacep.com.br: ", err)
+			} else {
+				fmt.Println("viacep.com.br: ")
+				fmt.Printf("%+v\n", address2)
+			}
+		}
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout")
+	}
 }
